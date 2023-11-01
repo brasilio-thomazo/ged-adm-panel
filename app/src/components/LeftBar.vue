bd6db456d66ddfc23ef69d23a73e6d9193b13344
Brasilio Thomazo <brasilio.thomazo@gmail.com>
2023-11-01 12:54:49 -0300

    update left-bar component

--- app/src/components/LeftBar.vue
+++ app/src/components/LeftBar.vue
@@ -21,7 +21,7 @@
               <RouterLink to="/apps">Registros</RouterLink>
             </li>
             <li v-for="app in apps">
-              <details class="submenu">
+              <details v-if="app.installed_at" class="submenu">
                 <summary>{{ app.path }}</summary>
                 <ul>
                   <li>
@@ -50,23 +50,23 @@
 </template>
 
 <script setup lang="ts">
-import { useStore } from "@/store/store";
-import axios from "axios";
-import { inject, onMounted, ref } from "vue";
+import { useStore } from '@/store/store'
+import axios from 'axios'
+import { inject, onMounted, ref } from 'vue'
 // import { useRouter } from "vue-router";
-const store = useStore();
-const http = inject("http", axios);
+const store = useStore()
+const http = inject('http', axios)
 
-const apps = ref<App[]>([]);
+const apps = ref<App[]>([])
 
 // const router = useRouter();
 
 onMounted(async () => {
   try {
-    const { data } = await http.get<App[]>("apps");
-    apps.value = data;
+    const { data } = await http.get<App[]>('apps')
+    apps.value = data
   } catch ({ response }: any) {
-    console.log(response);
+    console.log(response)
   }
-});
+})
 </script>
